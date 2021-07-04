dcd88e82ce15d371fe4f66c965e9f7acec287200
Leonardo Ascenzi <lascenzi@colgate.edu>
2021-07-03 23:41:32 -0400

    refresh heroku deploy

--- src/tasks/short/taskProcessData/index.js
+++ src/tasks/short/taskProcessData/index.js
@@ -1,7 +1,7 @@
 const task15ID = 15;
 const task15Name = 'Process Data';
 const task15Loc = 'Located in Office';
-const task15Desc = 'Hit the "Upload" button to upload data and wait about 10 seconds until upload is complete.';
+const task15Desc = 'Hit the "Upload" button to upload data and wait about 10 seconds until upload is complete';
 const task15TotalParts = 1;
 
 const taskProcessData = {
